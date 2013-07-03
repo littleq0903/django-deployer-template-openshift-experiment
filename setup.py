ac1edfeb360fdaa1332feaa31e95e37248ccc9fe
LittleQ <littleq0903@gmail.com>
2013-07-03 22:55:48 +0800

    first commit, and TODO: some parameters need to be templatize

---
+++ setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup, find_packages
+
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+setup(name='paasbakeoff',
+	version='1.0',
+	author='Nate Aune',
+	author_email='nate@appsembler.com',
+	url='https://github.com/appsembler/paasbakeoff',
+	packages=find_packages(),
+	include_package_data=True,
+	description='Example Mezzanine CMS deploy to OpenShift PaaS',
+    #TODO: need to be templatize
+	install_requires=open('%s/mywebsite/requirements/project.txt' % os.environ.get('OPENSHIFT_REPO_DIR', PROJECT_ROOT)).readlines(),
+#	install_requires=['Mezzanine==1.2.4',],
+)
+
