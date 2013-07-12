a290c88eabd0015ce794fe3cca85a69f8011d7a2
LittleQ <littleq0903@gmail.com>
2013-07-13 00:21:24 +0800

    location

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 from pip.req import parse_requirements
 
-REQUIREMENTS_FILE = os.path.join( os.path.dirname(__file__), 'requirements.openshift.txt')
+#REQUIREMENTS_FILE = os.path.join( os.path.dirname(__file__), 'requirements.openshift.txt')
 
 PROJECT_NAME = '<your-project-name>'
 AUTHOR_NAME = '<your-name>'
@@ -12,7 +12,7 @@ DESCRIPTION = '<your-project-description>'
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-REQUIREMENTS = [ str(ri.req) for ri in parse_requirements(REQUIREMENTS_FILE) ]
+#REQUIREMENTS = [ str(ri.req) for ri in parse_requirements(REQUIREMENTS_FILE) ]
 
 setup(name=PROJECT_NAME,
 	version='1.0',
@@ -22,5 +22,6 @@ setup(name=PROJECT_NAME,
 	packages=find_packages(),
 	include_package_data=True,
     description=DESCRIPTION,
-    install_requires=REQUIREMENTS)
+    #    install_requires=REQUIREMENTS)
+    )
 
